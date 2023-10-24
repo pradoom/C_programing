0614156c8e00dced0c09b75c9db9ca848166ec67
pradoom <pradoomrao@gmail.com>
2023-10-24 12:45:01 +0530

    preprocesser directive

--- #define_typedef/p1.c
+++ #define_typedef/p1.c
@@ -21,3 +21,73 @@
 // }
 
 //-----------------------------------------------------------------//
+
+// #define PROD(x,y) ((x)*(y))
+// int main(void)
+// {
+// int a=3,b=4;
+// printf("Product of a and b=%d",PROD(a,b));
+// return 0;
+// }
+
+
+// #define A 50
+// #define B A+100
+// int main(void)
+// {
+// int i,j;
+// i=B/20;//   A+100/20=55
+// j=500-B;//  500-B = 500-A+100 = 600-50=550
+// printf("i=%d,j=%d\n",i,j);
+// return 0;
+// }
+
+
+
+
+// #define ABS(x) ((x)<0?-(x):(x))
+// int main(void)
+// {
+// int array[4]={1,-2,3,-4};
+// int *p=array+3;
+// while(p>=array)//no error
+// {
+// printf("%d",ABS(*p));
+// p--;
+// return 0;
+// }
+// }
+
+
+
+
+// #define . ;//error: macro names must be identifiers
+// int main(void)
+// {
+// printf("If the lift to success is broken,").
+// printf("Try the stairs.").
+// }
+
+
+
+
+// #define CUBE(x) ((x)*(x)*(x)) //((i++)*(i++)*(i++))  1*2*3  = 6
+// int main(void)
+// {
+// int i=1;
+// while (i<=8)
+// printf ("%d\n", CUBE(i++)) ;// i=1
+// return 0;
+// }
+
+
+
+#define SWAP(dtype,x,y) { dtype t; t=x+y, x=t-x, y=t-y; }
+int main(void)
+{
+int a=1, b=2, x=3, y=4, s=25, t=26;
+SWAP (int, a, b)
+SWAP(int,x,y)
+SWAP (int, s, t)
+printf("a=%d, b=%d, x=%d, y=%d, s=%d, t=%d\n",a,b,x,y,s,t);
+}
--- struture/Structure_ouput_predication.c
+++ struture/Structure_ouput_predication.c
@@ -1,94 +1,92 @@
-#include<stdio.h>
-#include<string.h>
-#define op 11
-#if(op==1)
+#include <stdio.h>
+#include <string.h>
+#define op 20
+#if (op == 1)
 
 void main()
 {
-struct result
+    struct result
+    {
+        int marks;
+        char grade;
+    };
+    struct result Al, Bl;
+    Al.marks = 80;
+    Al.grade = 'A';
+    Bl = Al;
+    printf("Marks=%d\t", Bl.marks); // 80
+    printf("Grade=%c\n", Bl.grade); // A
+}
+
+#elif (op == 2)
+
+int main(void)
 {
-int marks;
-char grade;
+    struct rec
+    {
+        char *name;
+        int age;
+    } *ptr;
+    char name[10] = "Somalika";
+    ptr = (struct rec *)malloc(sizeof(struct rec));
+    ptr->name = name;
+    ptr->age = 93;
+    printf("%s\t", ptr->name); //"Somalika"
+    printf("%d\n", ptr->age);  // 93
+}
+
+#elif (op == 3)
+struct student
+{
+    char name[20];
+    int age;
 };
-struct result Al, Bl;
-Al.marks=80; Al.grade='A';
-Bl=Al;
-printf("Marks=%d\t",Bl.marks);//80
-printf("Grade=%c\n",Bl.grade);//A
-}
-
-
-
-
-#elif(op==2)
-
-
-int main (void)
-{
-struct rec
-{
-char *name;
-int age;
-}*ptr;
-char name[10]="Somalika";
-ptr=(struct rec *)malloc(sizeof(struct rec));
-ptr->name=name;
-ptr->age=93;
-printf ("%s\t" ,ptr->name);//"Somalika"
-printf("%d\n",ptr->age);   //93
-}
-
-
-
-#elif(op==3)
-struct student {char name[20];int age;};
 void main()
 {
-    struct student stu1={"Anita",10},stu2={"Anita",12};
-    if(stu1==stu2)//Error, since relational operators can't be used with structures. If we want to compare them then we'll have to compare all
-//the members one by one.
-    printf("Same\n");
+    struct student stu1 = {"Anita", 10}, stu2 = {"Anita", 12};
+    if (stu1 == stu2) // Error, since relational operators can't be used with structures. If we want to compare them then we'll have to compare all
+        // the members one by one.
+        printf("Same\n");
     else
-    printf("Not same\n");
+        printf("Not same\n");
     return 0;
 }
-#elif(op==4)
+#elif (op == 4)
 
 void func(struct tag v);
 
 int main(void)
 {
-struct tag
-{
-int i;
-char c;
-};//error should be declare globler
-struct tag var= {2,'s'};
-func(var);
+    struct tag
+    {
+        int i;
+        char c;
+    }; // error should be declare globler
+    struct tag var = {2, 's'};
+    func(var);
 }
 void func(struct tag v)
 {
-printf("%d %c\n",v.i,v.c);
+    printf("%d %c\n", v.i, v.c);
 }
 
-
-#elif(op==5)
+#elif (op == 5)
 void func(struct tag v);
 int main(void)
 {
-struct tag
-{
-int i;
-char c;
-};
-struct tag var= {2,'s'};
-func(var.i,var.c);
+    struct tag
+    {
+        int i;
+        char c;
+    };
+    struct tag var = {2, 's'};
+    func(var.i, var.c);
 }
-void func{struct {inti; char c;} v)
+void func(struct {inti; char c; } v)
 {
-printf("%d %c\n",v.i,v.c);
+    printf("%d %c\n", v.i, v.c);
 }
-#elif(op==6)
+#elif (op == 6)
 void main()
 {
     struct emp
@@ -99,14 +97,13 @@ void main()
         float bs;
     };
     struct emp e;
-   // e.name="Rahul";//error
-   e.s="rao";//valid
-   strcpy(e.name,"rahul");
-    e.age=25;
-    printf("%s %d %s",e.name,e.age,e.s);
-
+    // e.name="Rahul";//error
+    e.s = "rao"; // valid
+    strcpy(e.name, "rahul");
+    e.age = 25;
+    printf("%s %d %s", e.name, e.age, e.s);
 }
-#elif(op==7)
+#elif (op == 7)
 struct course
 {
     int courseno;
@@ -114,107 +111,276 @@ struct course
 };
 void main()
 {
-    struct course c[]={
+    struct course c[] = {
 
-        {102,"hello"},
-        {122,"rao"},
-        {324,"uroe"}
-    };
+        {102, "hello"},
+        {122, "rao"},
+        {324, "uroe"}};
 
-    printf("%d ",c[1].courseno);
-    printf("%s ",(*(c+2)).name);
+    printf("%d ", c[1].courseno);
+    printf("%s ", (*(c + 2)).name);
 }
-#elif(op==8)
+#elif (op == 8)
 void main()
 {
     struct value
     {
-            int bit1 :1;
-            int bit3 :4;
-            int bit4 :4;
-    }bit ={1,2,2};
-    printf("%d %d %d ",bit.bit1,bit.bit3,bit.bit4);//-1 2 2
+        int bit1 : 1;
+        int bit3 : 4;
+        int bit4 : 4;
+    } bit = {1, 2, 2};
+    printf("%d %d %d ", bit.bit1, bit.bit3, bit.bit4); //-1 2 2
 }
-#elif(op==9)
+#elif (op == 9)
 void main()
 {
-    //enum
-    enum day{mon=-1,tue,wed=6,thu,fri,sat};
-   // printf("%d %d %d %d %d",++mon,tue,wed,thu,fri,sat);//++mon error
-    printf("%d %d %d %d %d",1+mon,tue,wed,thu,fri,sat);//no error 0 0 6 7 8 
+    // enum
+    enum day
+    {
+        mon = -1,
+        tue,
+        wed = 6,
+        thu,
+        fri,
+        sat
+    };
+    // printf("%d %d %d %d %d",++mon,tue,wed,thu,fri,sat);//++mon error
+    printf("%d %d %d %d %d", 1 + mon, tue, wed, thu, fri, sat); // no error 0 0 6 7 8
 }
-#elif(op==10)
+#elif (op == 10)
 void main()
 {
-    enum status{pass,fail,atkt};
-    enum status s1,s2,s3;
-    s1=pass;
-    s2=fail;
-    s3=atkt;
-    printf("%d %d %d",s1,s2,s3);
-
+    enum status
+    {
+        pass,
+        fail,
+        atkt
+    };
+    enum status s1, s2, s3;
+    s1 = pass;
+    s2 = fail;
+    s3 = atkt;
+    printf("%d %d %d", s1, s2, s3);
 }
-#elif(op==11)
+#elif (op == 11)
 void main()
 {
     union
     {
-        struct {
-           char c[2];
+        struct
+        {
+            char c[2];
             char ch[2];
-        }s;
-        struct{
+        } s;
+        struct
+        {
 
             short int i;
             short int j;
-        }st;
-    }u={12,1,15,1};
-    printf("%d %d ",u.st.i,u.st.j);//268 271 how?
+        } st;
+    } u = {12, 1, 15, 1};
+    printf("%d %d ", u.st.i, u.st.j); // 268 271 how?
 }
-#elif(op==12)
+#elif (op == 12)
+
+struct sample
+{
+    int a;
+} sample;
+
+int main()
+{
+    sample.a = 100;         // No error
+    printf("%d", sample.a); // No error output is 100
+    return 0;
+}
+
+#elif (op == 13)
+
+struct sample
+{
+    int a;
+};
+
+int main()
+{
+
+    struct sample sample = {0, 9, 7}; // No error
+    // sample.a = 100;//No error
+    printf("%d", sample.a); // No error output is 100
+    return 0;
+}
+
+#elif (op == 14)
 void main()
 {
 
+    union test
+    {
+        int i;
+        int j;
+    };
+
+    union test var = 10; // Error invalid initializer
+    var.i = 10;          // No error
+    printf("%d, %d\n", var.i, var.j);
 }
-#elif(op==13)
-void main()
+#elif (op == 15)
+/*
+Assume that objects of the type short, float and long occupy 2 bytes, 4 bytes and 8 bytes,
+ respectively. The memory requirement for variable t, ignoring alignment considerations,
+ is (GATE CS 2000)
+*/
+// ans short s[5] will give 2x5=10  + long 8 bytes=18 bytes
+struct
 {
+    short s[5];
+    union
+    {
+        float y;
+        long z;
+    } u;
+} t;
+#elif (op == 16)
+
+/*
+
+Since x and arr[4] share the same memory, when we set x = 0, all characters of arr are set as 0.
+O is ASCII value of '\0'. When we do "t.arr[1] = 'G'", arr[] becomes "\0G\0\0". When we print a string using "%s",
+the printf function starts from the first character and keeps printing till it finds a \0. Since the first character
+itself is \0, nothing is printed.
+
+*/
+union test
+{
+    int x;
+    char arr[4];
+    int y;
+};
+
+int main()
+{
+    union test t;
+    t.x = 0;
+    t.arr[1] = 'G';
+    printf("%s", t.arr);
+    return 0;
+}
+
+#elif (op == 17)
+
+
+
+
+
+
+
+
+
+
+
+struct tag
+{
+    int i;
+    char c;
+};
+void func(struct tag);
+int main(void)
+{
+    struct tag var = {12, 'c'};
+    func(var);
+    printf("%d\n", var.i);
+    return 0;
 
 }
-#elif(op==14)
-void main()
+    void func(struct tag var)//function will not modify the structure value
+    {
+        var.i++;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#elif (op == 18)
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+
+struct tag
 {
+    int i;
+    char c;
+};
+void func(struct tag*);
+int main(void)
+{
+    struct tag var = {12, 'c'};
+    func(&var);//if we pass addres of structure it will modefy value in of structure memner
+    printf("%d\n", var.i);
+    return 0;
 
 }
-#elif(op==2)
-void main()
-{
+    void func(struct tag *var)
+    {
+        var->i++;
+        var->i++;
+    }
 
+
+
+#elif (op == 19)
+#include<string.h>
+#include<stdlib.h>
+typedef struct{char name[20]; int age;}stu;
+typedef struct{int data; node *link;}node;//error
+int main(void)
+{
+stu *p=malloc(sizeof(stu));
+node *ptr= malloc(sizeof(node));
+p->age=30;
+ptr->data=3;
+printf ("%d %d\n" ,p->age,ptr->data);
+return 0;
 }
-#elif(op==2)
-void main()
-{
 
-}
-#elif(op==2)
-void main()
-{
 
-}
-#elif(op==2)
-void main()
-{
 
-}
-#elif(op==2)
-void main()
-{
 
-}
-#elif(op==2)
-void main()
-{
 
+#elif (op == 20)
+#include<string.h>
+#include<stdlib.h>
+typedef struct{char name[20]; int age;}stu;
+typedef struct{int data;}node;//no error
+int main(void)
+{
+    
+stu *p=malloc(sizeof(stu));
+node *ptr= malloc(sizeof(node));
+p->age=30;
+ptr->data=3;
+printf ("%d %d\n" ,p->age,ptr->data);
+return 0;
 }
 
 #endif
