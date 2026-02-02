186190b9e2d38b72f446f7d8044655a603fac176
PRADOOM RAO <72682151+pradoom@users.noreply.github.com>
2026-02-03 00:43:41 +0530

    Update LEEDCODE_WORD_SERCH.c

--- Question/LEEDCODE_WORD_SERCH.c
+++ Question/LEEDCODE_WORD_SERCH.c
@@ -1,52 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 
-int dfs(char board[3][4], int rows, int cols,
+int dfs(char board[][4], int rows, int cols,
         int i, int j, char *word, int idx) {
-    if(word[idx]=='\0')
-    {
+
+    // Word completely matched
+    if (word[idx] == '\0')
         return 1;
-    }
-    if(i<0 || j<0 || i>=rows || j>=cols || board[i][j]=='#')
-    {
+
+    // Boundary and visited check
+    if (i < 0 || j < 0 || i >= rows || j >= cols || board[i][j] == '#')
         return 0;
-    }
-    if(board[i][j]!=word[idx])
-    {
+
+    // Character mismatch
+    if (board[i][j] != word[idx])
         return 0;
-    }
-    
+
+    // Mark current cell as visited
     char temp = board[i][j];
     board[i][j] = '#';
-    int dic[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
-    int k=0;
-    
-    for(k=0;k<4;k++)
-    {
-        int i_ = i+dic[k][0];
-        int j_ = j+dic[k][1];
-        
-        if(dfs(board,rows,cols,i_,j_,word,idx+1))
-        {
+
+    // Direction array: down, up, right, left
+    int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    // Explore all 4 directions
+    for (int k = 0; k < 4; k++) {
+        int new_i = i + dir[k][0];
+        int new_j = j + dir[k][1];
+
+        if (dfs(board, rows, cols, new_i, new_j, word, idx + 1))
             return 1;
-        }
     }
+
+    // Backtrack
     board[i][j] = temp;
-   return 0;
+
+    return 0;
 }
 
-int exist(char board[3][4], int rows, int cols, char *word) {
-
-    for(int i=0;i<rows;i++)
-    {
-        for(int j=0;j<cols;j++)
-        {
-            if(board[i][j]==word[0])
-            {
-                if(dfs(board,rows,cols,i,j,word,0))
-                {
+int exist(char board[][4], int rows, int cols, char *word) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (board[i][j] == word[0]) {
+                if (dfs(board, rows, cols, i, j, word, 0))
                     return 1;
-                }
             }
         }
     }
@@ -60,7 +57,7 @@ int main() {
         {'A','D','E','E'}
     };
 
-    char word[] = "SAE";
+    char word[] = "ABCCED";
 
     if (exist(board, 3, 4, word))
         printf("Word exists in board\n");
